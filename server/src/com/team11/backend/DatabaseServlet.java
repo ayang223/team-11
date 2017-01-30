22b9aa0a05a33c35a94992123f4ff3497725c9e8
Vincent Tung <vincentltung@gmail.com>
2017-01-29 21:04:03 -0800

    Added DatabaseHandler for methods

--- server/src/com/team11/backend/DatabaseServlet.java
+++ server/src/com/team11/backend/DatabaseServlet.java
@@ -1,5 +1,6 @@
 package com.team11.backend;
 
+import com.team11.backend.DatabaseHandler;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -27,7 +28,9 @@ public class DatabaseServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
+		
 		out.println("Testing Backend Server...");
+		out.println(DatabaseHandler.getUsers());
 	}
 
 	/**
