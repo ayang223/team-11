b5f9e04732436009bd6342eecc9df30698d3d800
Jack Li <jackubc2013@gmail.com>
2017-02-06 01:41:15 -0800

    Updated SQL setup script to incorporate new tables

--- database/United_Way_Init.sql
+++ database/United_Way_Init.sql
@@ -7,118 +7,140 @@ CREATE DATABASE IF NOT EXISTS United_Way_Andar_DB;
 
 USE United_Way_Andar_DB;
 
-CREATE TABLE Agencies (
+CREATE TABLE Agency (
 	id integer NOT NULL,
 	name varchar(128) NOT NULL,
-	description varchar(512) NOT NULL,
-	city varchar(128) NOT NULL,
-	longitude decimal(10,6) NOT NULL,
-	latitude decimal(10,6) NOT NULL,
-	
+	postal varchar(8) NOT NULL,
+
 	PRIMARY KEY (id)
 );
 
-/*file_name is the unique name given by the person adding the data*/
-/* tp: Target population
-   pe: Program event
-   mu: Municipality
-   de: Donor Engagement */
+CREATE TABLE Program (
+	id integer NOT NULL,
+	agency_andar integer NOT NULL,
+	name varchar(128) NOT NULL,
+	website varchar(128),
+	description varchar(512),
+	num_locations integer,
+
+	PRIMARY KEY (id),
+
+	FOREIGN KEY (agency_andar) REFERENCES Agency(id)
+);
+
+CREATE TABLE Location (
+	id integer NOT NULL AUTO_INCREMENT,
+	andar_id integer NOT NULL,
+	name varchar(128) NOT NULL,
+	postal varchar(8) NOT NULL,
+
+	PRIMARY KEY (id),
+
+	FOREIGN KEY (andar_id) REFERENCES Program(id)
+);
+
+/* file_name is the unique name given by the person adding the data */
 CREATE TABLE AndarDataOutput (
-	file_name varchar(32) NOT NULL UNIQUE,
+	file_name varchar(32) NOT NULL,
 	area varchar(32) NOT NULL,
 	funds varchar(16) NOT NULL,
 	focus varchar (32) NOT NULL,
 	outcome varchar (64) NOT NULL,
 	funding varchar (64) NOT NULL,
-	agency_andar Integer NOT NULL,
 	program_andar Integer NOT NULL,
-	program_name varchar(512) NOT NULL,
 	yearly_allocation Integer NOT NULL,
 	grant_start Date NOT NULL,
 	grant_end Date NOT NULL,
-	description  varchar(512),
+	description varchar(512),
 	planner varchar(128) NOT NULL,
-	
-	tp_early_childhood boolean,
-	tp_middle_years boolean,
-	tp_families boolean,
-	tp_youth boolean,
-	tp_seniors boolean,
-	tp_immigrants boolean,
-	tp_women boolean,
-	tp_aboriginal boolean,
-	tp_other_population boolean,
-	
-	pe_art boolean,
-	pe_caregiver boolean,
-	pe_provision boolean,
-	pe_security boolean,
-	pe_housing boolean,
-	pe_information boolean,
-	pe_intergenerational boolean,
-	pe_life_skills boolean,
-	pe_mental_wellness boolean,
-	pe_physical_activity boolean,
-	pe_physical_wellness boolean,
-	pe_policy boolean,
-	pe_research boolean,
-	pe_scholastic boolean,
-	pe_connections boolean,
-	pe_violence_prevention boolean,
-	
-	mu_anmore boolean,
-	mu_belcarra boolean,
-	mu_bowen_island boolean,
-	mu_burnaby boolean,
-	mu_coquitlam boolean,
-	mu_delta boolean,
-	mu_gibsons boolean,
-	mu_langley_city boolean,
-	mu_langley_district boolean,
-	mu_lions_bay boolean,
-	mu_maple_ridge boolean,
-	mu_new_westminster boolean,
-	mu_north_vancouver_city boolean,
-	mu_north_vancouver_district boolean,
-	mu_pemberton boolean,
-	mu_pitt_meadows boolean,
-	mu_port_coquitlam boolean,
-	mu_port_moody boolean,
-	mu_richmond boolean,
-	mu_sunshine_coast boolean,
-	mu_surrey boolean,
-	mu_squamish boolean,
-	mu_vancouver boolean,
-	mu_west_vancouver boolean,
-	mu_whistler boolean,
-	mu_white_rock boolean,
-	
-	de_UW boolean,
-	de_day_of_caring boolean,
-	de_volunteer boolean,
-	de_tour boolean,
-	de_fair boolean,
-	de_impact_stories boolean,
-	de_other boolean,
 
-	out_clients integer,
-	out_child_from_zero_to_six integer,
-	out_child_from_six_to_twelve integer,
-	out_children integer,
-	out_seniors integer,
-	out_guardian integer,
-	out_families integer,
-	out_contacts integer,
-	out_meals integer,
-	out_counselling integer,
-	out_mentors integer,
-	out_workshops integer,
-	out_volunteers integer,
-	out_locations integer,
+	PRIMARY KEY (program_andar),
 
-	PRIMARY KEY(program_andar),
+	FOREIGN KEY (program_andar) REFERENCES Program(id)
+);
 
-	FOREIGN KEY (agency_andar) REFERENCES Agencies(id)
+CREATE TABLE TargetPopulation (
+	andar_id integer NOT NULL,
+	population varchar(128) NOT NULL,
+
+	PRIMARY KEY (andar_id),
+
+	FOREIGN KEY (andar_id) REFERENCES Program(id)
+);
+
+CREATE TABLE ProgramElement (
+	andar_id integer NOT NULL,
+	element varchar(128) NOT NULL,
+	level integer NOT NULL,
+
+	PRIMARY KEY (andar_id),
+
+	FOREIGN KEY (andar_id) REFERENCES Program(id)
+);
+
+CREATE TABLE ProgramSubElement (
+	andar_id integer NOT NULL,
+	subElement varchar(128) NOT NULL,
+
+	PRIMARY KEY (andar_id),
+
+	FOREIGN KEY (andar_id) REFERENCES Program(id)
+);
+
+/* Maps Program Element and Program SubElement values */
+CREATE TABLE ElementDirectory (
+	element varchar(128) NOT NULL,
+	subElement varchar(128) NOT NULL,
+
+	PRIMARY KEY (element, subElement)
+);
+
+CREATE TABLE GeoArea (
+	andar_id integer NOT NULL,
+	area varchar(128) NOT NULL,
+	level integer NOT NULL,
+
+	PRIMARY KEY (andar_id),
+
+	FOREIGN KEY (andar_id) REFERENCES Program(id)
+);
+
+CREATE TABLE Municipality (
+	andar_id integer NOT NULL,
+	municipality varchar(256) NOT NULL,
+	focus_percentage integer NOT NULL,
+
+	PRIMARY KEY (andar_id),
+
+	FOREIGN KEY (andar_id) REFERENCES Program(id)
+);
+
+/* Maps Geo Area and Municipality values */
+CREATE TABLE AreaDirectory (
+	geoArea varchar(128) NOT NULL,
+	municipality varchar(256) NOT NULL,
+
+	PRIMARY KEY (geoArea, municipality)
+);
+
+CREATE TABLE DonorEngagement (
+	andar_id integer NOT NULL,
+	engagement varchar(128) NOT NULL,
+	description varchar(512),
+
+	PRIMARY KEY (andar_id),
+
+	FOREIGN KEY (andar_id) REFERENCES Program(id)
+);
+
+CREATE TABLE Outputs (
+	andar_id integer NOT NULL,
+	type varchar(128) NOT NULL,
+	value integer NOT NULL,
+
+	PRIMARY KEY (andar_id),
+
+	FOREIGN KEY (andar_id) REFERENCES Program(id)
 );
 
 /* Attribute AdminPrivileges identifies user
@@ -135,7 +157,7 @@ CREATE TABLE Users (
 	PRIMARY KEY (id)
 );
 
-/* Initialization of main administer account*/
+/* Initialization of main administrator account*/
 INSERT INTO Users
 VALUES (0, 'Main_Admin', 'main', NULL, NULL, TRUE);
 
