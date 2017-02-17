99bfdce93091504fc991915d239939883b033280
nathanmcsu <nathan.mc.su@gmail.com>
2017-02-16 17:26:00 -0800

    got rid of old code

--- client/src/scenes/Main.jsx
+++ client/src/scenes/Main.jsx
@@ -1,20 +1,6 @@
 var React = require('react');
 var Nav = require('Nav');
 
-// var Main = React.createClass({
-//   render: function () {
-//     return (
-//       <div>
-//         <Nav />
-//         <h2>Main Component</h2>
-//         {this.props.children}
-//       </div>
-//     );
-//   }
-// });
-//
-// module.exports = Main;
-
 class Main extends React.Component{
   constructor(props){
     super(props);
