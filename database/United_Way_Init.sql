4f657403c033cae2933296ba2992742cb4cd862e
Jack Li <jackubc2013@gmail.com>
2017-02-21 13:27:32 -0800

    Updated SQL setup script based on table rearrangement

--- database/United_Way_Init.sql
+++ database/United_Way_Init.sql
@@ -7,6 +7,110 @@ CREATE DATABASE IF NOT EXISTS United_Way_Andar_DB;
 
 USE United_Way_Andar_DB;
 
+/* file_name is the unique name given by the person adding the data */
+CREATE TABLE AndarDataOutput (
+	file_name varchar(32) NOT NULL,
+	area varchar(32) NOT NULL,
+	funds varchar(16) NOT NULL,
+	focus varchar (32) NOT NULL,
+	outcome varchar (64) NOT NULL,
+	funding varchar (64) NOT NULL,
+	program_andar integer NOT NULL,
+	yearly_allocation integer NOT NULL,
+	grant_start date NOT NULL,
+	grant_end date NOT NULL,
+	description varchar(512),
+	planner varchar(128) NOT NULL,
+
+	PRIMARY KEY (program_andar)
+);
+
+CREATE TABLE TargetPopulation (
+	andar_id integer NOT NULL,
+	population varchar(128) NOT NULL,
+
+	PRIMARY KEY (andar_id, population),
+
+	FOREIGN KEY (andar_id) REFERENCES AndarDataOutput(program_andar)
+);
+
+/* level is 100 = Primary, 200 = Secondary, 300 = N/A */
+CREATE TABLE ProgramElement (
+	andar_id integer NOT NULL,
+	element varchar(128) NOT NULL,
+	level integer NOT NULL,
+
+	PRIMARY KEY (andar_id, element),
+
+	FOREIGN KEY (andar_id) REFERENCES AndarDataOutput(program_andar)
+);
+
+CREATE TABLE ProgramSubElement (
+	andar_id integer NOT NULL,
+	subElement varchar(128) NOT NULL,
+
+	PRIMARY KEY (andar_id, subElement),
+
+	FOREIGN KEY (andar_id) REFERENCES AndarDataOutput(program_andar)
+);
+
+/* Maps Program Element and Program SubElement values */
+CREATE TABLE ElementDirectory (
+	element varchar(128) NOT NULL,
+	subElement varchar(128) NOT NULL,
+
+	PRIMARY KEY (element, subElement)
+);
+
+/* level is 100 = Yes/True, 200 = No/False */
+CREATE TABLE GeoArea (
+	andar_id integer NOT NULL,
+	area varchar(128) NOT NULL,
+	level integer NOT NULL,
+
+	PRIMARY KEY (andar_id, area),
+
+	FOREIGN KEY (andar_id) REFERENCES AndarDataOutput(program_andar)
+);
+
+CREATE TABLE Municipality (
+	andar_id integer NOT NULL,
+	municipality varchar(256) NOT NULL,
+	focus_percentage integer NOT NULL,
+
+	PRIMARY KEY (andar_id, municipality),
+
+	FOREIGN KEY (andar_id) REFERENCES AndarDataOutput(program_andar)
+);
+
+/* Maps Geo Area and Municipality values */
+CREATE TABLE AreaDirectory (
+	geoArea varchar(128) NOT NULL,
+	municipality varchar(256) NOT NULL,
+
+	PRIMARY KEY (geoArea, municipality)
+);
+
+CREATE TABLE DonorEngagement (
+	andar_id integer NOT NULL,
+	engagement varchar(128) NOT NULL,
+	description varchar(512),
+
+	PRIMARY KEY (andar_id, engagement),
+
+	FOREIGN KEY (andar_id) REFERENCES AndarDataOutput(program_andar)
+);
+
+CREATE TABLE Outputs (
+	andar_id integer NOT NULL,
+	type varchar(128) NOT NULL,
+	value integer NOT NULL,
+
+	PRIMARY KEY (andar_id, type),
+
+	FOREIGN KEY (andar_id) REFERENCES AndarDataOutput(program_andar)
+);
+
 CREATE TABLE Agency (
 	id integer NOT NULL,
 	name varchar(128) NOT NULL,
@@ -25,6 +129,7 @@ CREATE TABLE Program (
 
 	PRIMARY KEY (id),
 
+	FOREIGN KEY (id) REFERENCES AndarDataOutput(program_andar),
 	FOREIGN KEY (agency_andar) REFERENCES Agency(id)
 );
 
@@ -39,115 +144,11 @@ CREATE TABLE Location (
 	FOREIGN KEY (andar_id) REFERENCES Program(id)
 );
 
-/* file_name is the unique name given by the person adding the data */
-CREATE TABLE AndarDataOutput (
-	file_name varchar(32) NOT NULL,
-	area varchar(32) NOT NULL,
-	funds varchar(16) NOT NULL,
-	focus varchar (32) NOT NULL,
-	outcome varchar (64) NOT NULL,
-	funding varchar (64) NOT NULL,
-	program_andar Integer NOT NULL,
-	yearly_allocation Integer NOT NULL,
-	grant_start Date NOT NULL,
-	grant_end Date NOT NULL,
-	description varchar(512),
-	planner varchar(128) NOT NULL,
-
-	PRIMARY KEY (program_andar),
-
-	FOREIGN KEY (program_andar) REFERENCES Program(id)
-);
-
-CREATE TABLE TargetPopulation (
-	andar_id integer NOT NULL,
-	population varchar(128) NOT NULL,
-
-	PRIMARY KEY (andar_id, population),
-
-	FOREIGN KEY (andar_id) REFERENCES Program(id)
-);
-
-CREATE TABLE ProgramElement (
-	andar_id integer NOT NULL,
-	element varchar(128) NOT NULL,
-	level integer NOT NULL,
-
-	PRIMARY KEY (andar_id, element),
-
-	FOREIGN KEY (andar_id) REFERENCES Program(id)
-);
-
-CREATE TABLE ProgramSubElement (
-	andar_id integer NOT NULL,
-	subElement varchar(128) NOT NULL,
-
-	PRIMARY KEY (andar_id, subElement),
-
-	FOREIGN KEY (andar_id) REFERENCES Program(id)
-);
-
-/* Maps Program Element and Program SubElement values */
-CREATE TABLE ElementDirectory (
-	element varchar(128) NOT NULL,
-	subElement varchar(128) NOT NULL,
-
-	PRIMARY KEY (element, subElement)
-);
-
-CREATE TABLE GeoArea (
-	andar_id integer NOT NULL,
-	area varchar(128) NOT NULL,
-	level integer NOT NULL,
-
-	PRIMARY KEY (andar_id, area),
-
-	FOREIGN KEY (andar_id) REFERENCES Program(id)
-);
-
-CREATE TABLE Municipality (
-	andar_id integer NOT NULL,
-	municipality varchar(256) NOT NULL,
-	focus_percentage integer NOT NULL,
-
-	PRIMARY KEY (andar_id, municipality),
-
-	FOREIGN KEY (andar_id) REFERENCES Program(id)
-);
-
-/* Maps Geo Area and Municipality values */
-CREATE TABLE AreaDirectory (
-	geoArea varchar(128) NOT NULL,
-	municipality varchar(256) NOT NULL,
-
-	PRIMARY KEY (geoArea, municipality)
-);
-
-CREATE TABLE DonorEngagement (
-	andar_id integer NOT NULL,
-	engagement varchar(128) NOT NULL,
-	description varchar(512),
-
-	PRIMARY KEY (andar_id, engagement),
-
-	FOREIGN KEY (andar_id) REFERENCES Program(id)
-);
-
-CREATE TABLE Outputs (
-	andar_id integer NOT NULL,
-	type varchar(128) NOT NULL,
-	value integer NOT NULL,
-
-	PRIMARY KEY (andar_id, type),
-
-	FOREIGN KEY (andar_id) REFERENCES Program(id)
-);
-
 /* Attribute AdminPrivileges identifies user
 	False: Basic user
 	True: Admin user*/
 CREATE TABLE Users (
-	id integer NOT NULL,
+	id integer NOT NULL AUTO_INCREMENT,
 	username varchar(32) NOT NULL,
 	password varchar(32) NOT NULL,
 	first_name varchar(32),
