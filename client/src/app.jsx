5d31a76d95fe1d016b46cb03aa96c06ae663a8a8
nathanmcsu <nathan.mc.su@gmail.com>
2017-03-03 12:41:23 -0800

    authencation frontend works, routes being validated

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,25 +1,34 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var {Route, Router, IndexRoute, hashHistory, RouterContext} = require('react-router');
 var Main = require('Main');
 var Login = require('Login');
 var Account = require('Account');
 var Import = require('Import');
 var Dashboard = require('Dashboard');
 var Test = require('src/scenes/Test.jsx');
+import cookie from 'react-cookie';
 require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
 
+function validateSession(nextState, replace, callback) {
+    if (typeof cookie.load('userID') === "undefined") {
+        hashHistory.push('/login');
+        console.log("blocked");
+    }else{
+      console.log("validated");
+      callback();
+    }
+}
+
 ReactDOM.render(
-  <Router history={hashHistory}>
+    <Router history={hashHistory}>
     <Route path="/" component={Main}>
-      <Route path="/login" component={Login}/>
-      <Route path="/account" component={Account}/>
-      <Route path="/import" component={Import}/>
-      <Route path="/dashboard" component={Dashboard}/>
-      <Route path="/test" component={Test}/>
-      <IndexRoute component={Login}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/account" onEnter={validateSession} component={Account} />
+        <Route path="/import"  onEnter={validateSession} component={Import} />
+        <Route path="/dashboard"  onEnter={validateSession} component={Dashboard}/>
+        <Route path="/test" component={Test}/>
+        <IndexRoute component={Login}/>
     </Route>
-  </Router>,
-  document.getElementById('app')
-);
+</Router>, document.getElementById('app'));
