f7e8d34b49fbd90c768d71225510eaf0cfe15d3f
ayang223 <a.yang223@gmail.com>
2017-03-05 23:19:37 -0800

    added adminminpage

---
+++ client/src/scenes/AdminPage.jsx
@@ -0,0 +1,47 @@
+var React = require('react');
+
+var getAdminPage = React.createClass({
+  getUsers:function(){
+    $a.jax({
+      url:"http://localhost:8080/BackendServer/DatabaseServlet",
+      type: "POST",
+      data: JSON.stringify({
+      	"action": "List User"
+      }),
+      dataType: "json",
+      succuess:function(data){
+        console.log(data)
+        document.getElementById('out').innerHTML = JSON.stringify(data);
+        console.log("success");
+      }.bind(this),
+      error:function(error){
+        document.getElementById('out').innerHTML = error;
+        console.log("error");
+      }
+    });
+  },
+
+  createUser:function(){
+    return(
+      <div>
+      <p>create user page</p>
+      </div>
+    )
+  },
+
+  render:function(){
+    return(
+      <div>
+      <input type="button" ref="button" value="Get Users" onClick={this.getUsers}/>
+      <div id="out">
+      </div>
+      <div>
+      {createUser}
+      </div>
+      </div>
+      )
+  }
+});
+
+
+module.exports = AdminPage;
