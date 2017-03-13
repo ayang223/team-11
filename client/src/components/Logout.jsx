74dab184d14d300df2610c54a41a8904852aecf9
viviancpsc <jlzhang1995@gmail.com>
2017-03-13 02:59:44 -0700

    log out pop out

--- client/src/components/Logout.jsx
+++ client/src/components/Logout.jsx
@@ -4,19 +4,36 @@ var {hashHistory} = require('react-router');
 
 class Logout extends React.Component{
   logoutSess(){
-    var sure = window.confirm("Are you sure?")
-    if(sure){
+    //var sure = window.confirm("Are you sure?")
+    //if(sure){
       cookie.remove('userID', []);
       cookie.remove('admin', []);
       hashHistory.push('/login');
-    }
+  //  }
   }
+
   render(){
     return(
-    <div>
-      <h2>Logout Page</h2>
-      <button type="button" className="alert button" onClick={this.logoutSess}>Logout</button>
-    </div>
+  <div >
+    <div className="modal fade" id="loginModal" role="dialog" tabIndex="-1">
+      <div className= "modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+               <button type="button" className="btn btn-default" data-dismiss="modal" >&times;</button>
+              <h3 className="modal-title"> Logout </h3>
+              <div className="modal-body">
+                <p>Are you sure you want to log out?</p>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="alert button" onClick={this.logoutSess}>Confirm</button>
+                <button type="button" className="button small-centered text-center coloumns" data-dismiss="modal"  aria-label="Close">Close</button>
+
+                </div>
+          </div>
+            </div>
+          </div>
+        </div>
+      </div>
   )
   }
 }
