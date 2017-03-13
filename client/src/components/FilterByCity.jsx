039a8a69823e940a721f96c8e23a76747b10158f
Alex Yang <a.yang223@gmail.com>
2017-03-12 22:29:39 -0700

    filters for population and city done

--- client/src/components/FilterByCity.jsx
+++ client/src/components/FilterByCity.jsx
@@ -19,18 +19,31 @@ var FilterByCity = React.createClass({
       array.push(JSON.stringify(e.target.value));
     }
   },
+
+  createMetaData:function(data){
+    var metadata = {};
+    var city = data.Municipality;
+    var cityArr = []; 
+    for (var i =0; i< city.length; i++){
+      if(cityArr.includes(city[i].municipality)){
+        console.log("population true")
+      } else cityArr.push(city[i].municipality)
+    }
+    return cityArr;
+  },
+
   render:function(){
+    var dataFromDash = this.props.data;
     var message = 'FilterByCity: ' + this.state.selectValue;
+    var cityArr = this.createMetaData(dataFromDash);
+    const listItems = cityArr.map((municipality) =>
+      <option key={municipality} value={municipality} style={{margin:"2px"}}>{municipality}</option>
+      );
     return(
       <div className="medium-3 columns">
         <label>Select city</label>
       <select multiple={{true}} size="3" value={[]} onChange={this.handleChange}>
-          <option value="Vancouver">Vancouver</option>
-          <option value="Richmond">Richmond</option>
-          <option value="Burnaby">Burnaby</option>
-          <option value="Surrey">Surrey</option>
-          <option value="Delta">Delta</option>
-          <option value="Langely">Langely</option>
+          {listItems}
         </select>
         <label>{message}</label>
       </div>
