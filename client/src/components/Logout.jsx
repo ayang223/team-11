fba9b4106334e0d0a8e2e50f7e4d841bcc83ce30
viviancpsc <jlzhang1995@gmail.com>
2017-03-15 12:34:30 -0700

    modal css and close event

--- client/src/components/Logout.jsx
+++ client/src/components/Logout.jsx
@@ -3,6 +3,11 @@ import cookie from 'react-cookie';
 var {hashHistory} = require('react-router');
 
 class Logout extends React.Component{
+
+  close() {
+    alert("Close the modal");
+  }
+
   logoutSess(){
     //var sure = window.confirm("Are you sure?")
     //if(sure){
@@ -15,18 +20,18 @@ class Logout extends React.Component{
   render(){
     return(
   <div >
-    <div className="modal fade" id="loginModal" role="dialog" tabIndex="-1">
+    <div className="modal fade" id="loginModal" role="dialog" tabIndex="-1" >
       <div className= "modal-dialog">
         <div className="modal-content">
           <div className="modal-header">
-               <button type="button" className="btn btn-default" data-dismiss="modal" >&times;</button>
+               <button type="button" className="btn btn-default" data-dismiss="modal"  aria-hidden="true" onClick={this.close}>&times;</button>
               <h3 className="modal-title"> Logout </h3>
               <div className="modal-body">
                 <p>Are you sure you want to log out?</p>
               </div>
               <div className="modal-footer">
                 <button type="button" className="alert button" onClick={this.logoutSess}>Confirm</button>
-                <button type="button" className="button small-centered text-center coloumns" data-dismiss="modal"  aria-label="Close">Close</button>
+                <button type="button" className="button large-centered text-center coloumns" data-dismiss="modal"  aria-label="Close" onClick={this.close}>Close</button>
 
                 </div>
           </div>
@@ -34,6 +39,7 @@ class Logout extends React.Component{
           </div>
         </div>
       </div>
+
   )
   }
 }
