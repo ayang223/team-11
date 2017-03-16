bd8fc346ffa37234e686cdf93755ba694d1c9ea1
nathanmcsu <nathan.mc.su@gmail.com>
2017-03-15 21:58:30 -0700

    timeout for d3

--- client/src/components/d3map.jsx
+++ client/src/components/d3map.jsx
@@ -12,7 +12,7 @@ import PlacesAutocomplete, { geocodeByAddress} from 'react-places-autocomplete';
   var width = 700;
   var height = 700;
   var scale = 100000 * 5;
-  var scaleExtent = [1 << 12, 1 << 13]
+  var scaleExtent = [1 << 12, 1 << 30]
   var center = [-123.1022025, 49.2823492];
   var mapData = {
           "type": "Feature",
@@ -21,9 +21,20 @@ import PlacesAutocomplete, { geocodeByAddress} from 'react-places-autocomplete';
           },
           "geometry": {
               "type": "Point",
-              "coordinates": [0, 0]
+              "coordinates": [[-123.1009434, 49.2826161], [-122.98590580000001, 49.2300456]]
           }
       }
+ var data = {
+   'geometry':
+   {
+     coordinates: [[[-74.0479, 40.8820], [-73.9067, 40.8820], [-73.9067, 40.6829], [-74.0479, 40.6829], [-74.0479, 40.8820]]],
+     type: "Point"
+    },
+    id: 999999, properties:
+    {
+      "text": "hi, this is a polygon!"
+    }, type: "Feature"};
+
 var popupContent = function(d) { return 'hi, i am polygon'; }
 
 var onPolygonClick= function(e, d, i) {
@@ -32,7 +43,28 @@ var onPolygonClick= function(e, d, i) {
 var onPolygonCloseClick= function(e, id) {
     e.hidePopup();
   }
+var counter = 0;
+var latLong = [];
 var D3Map = React.createClass({
+  doTimeout : function(ps, n){
+    var _this = mapData;
+    setTimeout(function(){
+        geocodeByAddress(ps, (err, result) => {
+          if(err){
+            console.log(err);
+          }else{
+            //console.log("lng: " +result.lng);
+            //console.log("lat: " +result.lat);
+            latLong.push([result.lng, result.lat]);
+            _this.geometry.coordinates.push(latLong[n]);
+            console.log( _this.geometry.coordinates)
+            //return result;
+            //console.log(result);
+            //console.log(latLong);
+          }
+        });
+      }, 1000 * n);
+  },
   createPostalCodes: function(data){
     var _this = mapData;
     var locations = data.Location;
@@ -42,14 +74,11 @@ var D3Map = React.createClass({
     for(var i = 0; i< locations.length; i++){
       postalCodes.push(locations[i].postal)
     }
-    //console.log(postalCodes);
-    geocodeByAddress("V6E 2X5", (err, result) => {
-      if(err){
-        console.log(err);
+    console.log(postalCodes);
+    for(var i = 0; i < postalCodes.length; i++){
+      //this.doTimeout(postalCodes[i], i);
       }
-      console.log(result);
-      _this.geometry.coordinates = [result.lng, result.lat];
-    });
+      //_this.geometry.coordinates = [result.lng, result.lat];
   },
   render() {
     var dataFromDash = this.props.data;
@@ -66,7 +95,7 @@ var D3Map = React.createClass({
       >
         <MarkerGroup
           key= {"polygon-test"}
-          data= {mapData}
+          data= {data}
           popupContent= {popupContent}
           onClick= {onPolygonClick}
           onCloseClick= {onPolygonCloseClick}
