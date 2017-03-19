9022e4637c7b06588e0f9f35a9feac157b97f0f4
Alex Yang <a.yang223@gmail.com>
2017-03-18 19:24:15 -0700

    filters by years done

--- client/src/components/FilterByYear.jsx
+++ client/src/components/FilterByYear.jsx
@@ -50,15 +50,40 @@ var FilterByYear = React.createClass({
   //  console.log( "years: " + yearArr);
     return yearArr;
   },
+  createMetadata : function(data){
+    var metadata = {};
+    var year = data.AndarDataOutput;
+    var yearArr = [];
+    for(var i =0; i< year.length; i++){
+      if(yearArr.includes (year[i].grant_date.substring(0,4) + "/" +year[i].grant_end.substring(0,4))){
+        console.log("years already contained in array")
+      } else yearArr.push(year[i].grant_date.substring(0,4) + "/" +year[i].grant_end.substring(0,4))
+    }
+    console.log( "years: " + yearArr);
+    return yearArr;
+  },
 
   render: function(){
     var dataFromDash = this.props.data;
     var message = 'FilterByYear: ' + this.state.selectValue;
     var yearArr = this.createMetadata(dataFromDash);
+<<<<<<< HEAD
     return(
       <div>
           <label>Funding Year Filter</label>
           <Select placeholder="Select Funding Year" multi disabled={this.state.disabled} value={this.state.value} options={yearArr} onChange={this.handleChange}/>
+=======
+    const listItems = yearArr.map((grant_date) =>
+      <option key={grant_date} value={grant_date} style={{margin:"2px"}}>{grant_date}</option>
+      );
+    return(
+      <div className="medium-3 columns">
+        <label>Select year</label>
+        <select multiple={{true}} size="3" value={[]} onChange={this.handleChange}>
+          {listItems}
+        </select>
+        <label>{message}</label>
+>>>>>>>  filters by years done
       </div>
     )
   },
