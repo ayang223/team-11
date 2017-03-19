50e2b18f15a844ac88d903773e5375b9dacd0abc
Alex Yang <a.yang223@gmail.com>
2017-03-18 19:24:15 -0700

    filters by years done

--- client/src/scenes/Dashboard.jsx
+++ client/src/scenes/Dashboard.jsx
@@ -66,7 +66,7 @@ class Dashboard extends React.Component {
                     }}>Dashboard Page</h2>
                     <br/>
                     <div className="row">
-                        <FilterByYear/>
+                        <FilterByYear data={this.state.data}/>
                         <FilterByCity data={this.state.data}/>
                         <FilterByInvested data={this.state.data}/>
                         <FilterByAgency data={this.state.data}/>
