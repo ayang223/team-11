d3695a9092113265db2e807433b22b3c7666cc99
Alex Yang <a.yang223@gmail.com>
2017-03-18 19:24:15 -0700

    filters by years done

--- client/src/components/FilterByInvested.jsx
+++ client/src/components/FilterByInvested.jsx
@@ -30,31 +30,31 @@ var FilterByInvested = React.createClass({
     for(var i=0;i < investedArr.length; i++){
       if(investedArr[i] < 5000){
         if(investedFilterArr.includes("less than $5000")){
-          console.log("invest true")
+          console.log("less than $5000 exists")
         } else investedFilterArr.push("less than $5000");
       } else if(investedArr[i] >= 5000 && investedArr[i] <= 10000){
         if(investedFilterArr.includes("5000-$10000")){
-          console.log("invest true")
+          console.log("5000-$10000 exists")
         } else investedFilterArr.push("$5000-$10000");
       } else if (investedArr[i] >= 10000 && investedArr[i] <= 50000){
-        if(investedFilterArr.includes("10000-$50000")){
-          console.log("invest true")
+        if(investedFilterArr.includes("$10000-$50000")){
+          console.log("10000-$50000 exists")
         } else investedFilterArr.push("$10000-$50000");
       } else if (investedArr[i] >= 50000 && investedArr[i] <= 100000){
         if(investedFilterArr.includes("$50000-$100000")){
-          console.log("invest true")
+          console.log("$50000-$100000 exists")
         } else investedFilterArr.push("$50000-$100000");
       }else if (investedArr[i] >= 100000 && investedArr[i] <= 250000){
         if(investedFilterArr.includes("$100000-$250000")){
-          console.log("invest true")
+          console.log("100000-$250000 exists")
         } else investedFilterArr.push("$100000-$250000");
       }else if (investedArr[i] >= 250000 && investedArr[i] <= 500000){
         if(investedFilterArr.includes("250000-$500000")){
-          console.log("invest true")
+          console.log("250000-$500000 exixts")
         } else investedFilterArr.push("$250000-$500000");
       }else if (investedArr[i] >= 500000 && investedArr[i] <= 1000000){
         if(investedFilterArr.includes("$500000-$1000000")){
-          console.log("invest true")
+          console.log("$500000-$1000000 exists")
         } else investedFilterArr.push("$500000-$1000000");
       }
     }
