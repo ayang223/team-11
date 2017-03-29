0b22a32dd9b7f938ef55814263b8a6f1f65405eb
Alex Yang <a.yang223@gmail.com>
2017-03-28 18:16:23 -0700

    dynamically gets geo area

--- client/src/components/FilterByGeoArea.jsx
+++ client/src/components/FilterByGeoArea.jsx
@@ -1,10 +1,15 @@
 var React = require('react');
 var array = [];
+var Select = require('react-select');
 
 var FilterByGeoArea = React.createClass({
 
 	getInitialState:function(){
-		return{value: [], disabled: false, selectValue: []}
+		return{
+			value: [], 
+			disabled: false, 
+			selectValue: [],
+		};
 	},
 
 	handleChange:function(value){
@@ -23,8 +28,41 @@ var FilterByGeoArea = React.createClass({
 		var metadata = {};
 		var geoArea = data.GeoArea;
 		var geoAreaArr = [];
-		
-	}
+		for (var i = 0; i < geoArea.length; i++){
+			var object = {
+				label: "",
+				value: ""
+			}
+			object.label = geoArea[i].area;
+			object.value = geoArea[i].area;
+			var isDup = false;
+			for(var j = 0; j < geoAreaArr.length; j++){
+				if(object.value == geoAreaArr[j].value){
+					isDup = true;
+					break;
+				}
+			}
+			if(isDup){
+				console.log("dupe in geo area");
+			} else {
+				geoAreaArr.push(object)
+			}
+		}
+		return geoAreaArr;
+	},
 
+	render:function(){
+    var dataFromDash = this.props.data;
+    var message = 'FilterByGeoArea: ' + this.state.selectValue;
+    var geoAreaArr = this.createMetadata(dataFromDash);
 
+    return(
+      <div>
+        <label>Geo Area Filter</label>
+        <Select placeholder="Select Geo Area" multi disabled={this.state.disabled} value={this.state.value} options={geoAreaArr} onChange={this.handleChange}  />
+        </div>
+    )
+  }
 })
+
+module.exports = FilterByGeoArea;
